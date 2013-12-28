f4042302ff46cd6a78bdc9591133c39bcdc187af
Alexander Luksidadi <alexander.luksidadi@veevasystems.com>
2013-12-27 21:58:45 -0500

    oauth config

--- app/config/app.php
+++ app/config/app.php
@@ -106,6 +106,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+        'Artdarek\OAuth\OAuthServiceProvider',
 
 	),
 
@@ -172,6 +173,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+        'OAuth'           => 'Artdarek\OAuth\Facade\OAuth',
 
 	),
 
